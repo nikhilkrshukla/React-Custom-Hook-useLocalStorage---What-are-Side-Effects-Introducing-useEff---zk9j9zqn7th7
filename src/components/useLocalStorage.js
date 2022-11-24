0ef1fb1c15e24b8b3950102ecf7701598e753f9a
Nikhil Shukla <51776266+nikhilkrshukla@users.noreply.github.com>
2022-11-24 14:21:32 +0530

    useLocalStorage update (#1)

--- src/components/useLocalStorage.js
+++ src/components/useLocalStorage.js
@@ -2,7 +2,13 @@ import { useEffect } from "react";
 import { useState } from "react";
 
 const useLocalStorage = (stateVar, value) => {
-  
+  const [state, setState] = useState(value);
+
+  useEffect(() => {
+    localStorage.setItem(stateVar, state);
+  }, [state]);
+
+  return [state, setState];
 };
 
 export default useLocalStorage;
